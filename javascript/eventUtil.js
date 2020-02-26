a8cda3c3ed61220d7a3494351953eb17d69e9701
Barkley.Chang <changan25801@163.com>
2020-02-26 18:33:14 +0800

    update files

---
+++ javascript/eventUtil.js
@@ -0,0 +1,56 @@
+﻿var EventUtil = {
+        // 添加事件
+        addEvent: function(element, eventName, fn){
+            if(element.addEventListener){
+                element.addEventListener(eventName, fn, false);
+            }else if(element.attachEvent){
+                element.attachEvent('on' + eventName, fn)
+            }else{
+                element.eventName = fn;
+            }
+        },
+
+        // 移除事件
+        removeEvent: function(element, eventName, fn){
+            if(element.removeEventListener){
+                element.removeEventListener(eventName, fn, false);
+            }else if (element.deleteEvent){
+                element.deleteEvent('on' + eventName, fn);
+            }else{
+                element.eventName = null;
+            }
+        },
+
+        // 获取事件对象
+        getEvent: function(event){
+            return event?event:window.event;
+        },
+
+        // 获取事件类型
+        getEventType: function(event){
+            return event.type;
+        },
+
+        // 获取被执行事件的目标元素
+        getEventTarget: function(event){
+            return event.target | event.srcElement;
+        },
+
+        // 禁用元素的默认行为
+        preventDefault: function(event){
+            if(event.preventDefault){
+                event.preventDefault();
+            }else{ //IE8以及更低版本
+                event.returnValue = false;
+            }
+        },
+
+        // 阻止元素冒泡
+        stopPropagation: function(event){
+            if(event.stopPropagation){                    
+                event.stopPropagation();
+            }else{ //IE8以及更s低版本
+                event.cancelable = true;
+            }
+        }
+    }; 
--- vscode/note.md
+++ vscode/note.md
@@ -1,6 +1,6 @@
 - 下载路径
 
-    https://code.visualstudio.com  
+    <https://code.visualstudio.com>  
 
 - 安装注意事项
     取消勾选“将Code注册为受支持的文件类型编辑器”,其余全部勾选  
@@ -11,15 +11,42 @@
     
     Live Server 在页面上右键 -> Open with Live Server
     
-    ​    设置自动保存: 文件 -> 首选项 -> 设置 -> Files:Auto Save: afterDelay 1000
+    ​    设置自动保存: 文件 -> 首选项 -> 设置 -> Files:Auto Save: afterDelay 1000  
+    
+    Beautify  格式化html、css、js文件
+    
+    Easy LESS  
+    
+    ​    设置less与css文件映射: 管理 -> 设置 -> 扩展 -> Easy LESS configuration -> 在settings.json中编辑 -> 加入如下配置(会生成xxx.css.map文件):  
+    
+    ```less
+    "less.compile": {
+             "compress":  false,  // true => remove surplus whitespace
+             "sourceMap": true,  // true => generate source maps (.css.map files)
+             "out":       true, // false => DON'T output .css files (overridable per-file, see below)
+    }
+    ```
+    
+    JavaScript (ES6) code snippets 代码提示
     
 - 设置
 
     1. 取消迷你地图: 查看 -> 取消勾选"显示迷你地图"
     2. 取消面包屑: 查看 -> 取消勾选"显示面包屑"
     3. 渲染空格: 查看 -> 勾选"渲染空格" 
+    4. 修改提示快捷键 文件 - > 首选项 - > 键盘快捷方式 - > 修改"触发建议"
 
 - 快捷键
     1. ctrl + / 注释
-    2. ! + tab html基本结构
+    2. alt + shift + a 块区域注释
+    3. ! + tab html基本结构
+    4. alt + shift + 上|下 向上复制一行|向下复制一行
+    5. alt + shift + f 格式化文档
+    6. F2 修改文件名
+    7. alt + 向上箭头|向下箭头 当前行向上移动|当前行向下移动
+    8. ctrl + shift + k 删除一行
+    9. ctrl + shift + p 显示全局命令面板
+    10. ctrl + [|] 行缩进
+    11. ctrl + shift + p 显示全局命令面板
+    12. alt + 鼠标左键 选中多个区域编辑
 
